a79cf7f35c549af50ec8b29be19d10aa91950482
rohit-56 <rohitmou25@gmail.com>
2022-11-02 12:19:03 +0530

    add api config

--- WeatherApp/Controller/WeatherViewController.swift
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -8,22 +8,60 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+     
+    @IBOutlet weak var CityName: UILabel!
+    @IBOutlet weak var Temp: UILabel!
+    
+    @IBOutlet weak var Description: UILabel!
+    
+    @IBOutlet weak var min: UILabel!
+    
+    @IBOutlet weak var max: UILabel!
+    
+    var tempe : String?
+    var weather  : WeatherResponse?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+     
+       // CityName.text = "Delhi"
+      //  Temp.text = tempe
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    @IBOutlet weak var search: UISearchBar!
+    func getData() {
+            
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=c6ddd223cc502984b33546fe5f13361b&units=metric") else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let dataTask =  URLSession.shared.dataTask(with: request) { [self] data, response, error in
+            guard data != nil else {
+                print("data is nil")
+                return
+            }
+            let decoder = JSONDecoder()
+            
+            do{
+                let result = try decoder.decode(WeatherResponse.self,from: data!)
+                weather = result
+                tempe = String(format: "%f", result.main.temp)
+                print(result.weather.description)
+                print("hello")
+            }catch{
+                print("Error while parsing JSON data")
+            }
+            
+           
+        
+        }
+        dataTask.resume()
+        }
 
 }
--- WeatherApp/Modal/WeatherResponse.swift
+++ WeatherApp/Modal/WeatherResponse.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct WeatherResponse : Codable {
+    let name : String
+    let weather : [WeatherAPI]
+    let main : MainAPI
+}
+
+struct WeatherAPI : Codable {
+    let description : String
+    let main : String
+}
+
+struct MainAPI : Codable {
+    let temp : Double
+    let pressure : Double
+    let humidity : Double
+    let temp_min : Double
+    let temp_max : Double
+    let feels_like : Double
+}
